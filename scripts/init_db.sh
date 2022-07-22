4e18e2b59039c5f0ae2a48a5b57fcd365065800a
Alexander Goussas <aloussase@tosca.aloussase.dev>
2022-07-22 16:36:33 -0500

    Use docker-compose for containerizing the application
    For local devlopment and testing the container has to be run with
    `APP_ENVIRONMENT=local` and the test command must be
    `APP_ENVIRONMENT=testing cargo test`.
    
    This is because we need a different host for connecting to the database
    for testing than the one used by the server inside the container.

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -26,27 +26,6 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 # Check if a custom host has been set, otherwise default to 'localhost'
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
-# Allow to skip Docker if a dockerized Postgres database is already running
-if [[ -z "${SKIP_DOCKER}" ]]; then
-  # if a postgres container is running, print instructions to kill it and exit
-  RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
-  if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
-    echo >&2 "there is a postgres container already running, kill it with"
-    echo >&2 "    docker kill ${RUNNING_POSTGRES_CONTAINER}"
-    exit 1
-  fi
-  # Launch postgres using Docker
-  docker run \
-    -e POSTGRES_USER=${DB_USER} \
-    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_DB=${DB_NAME} \
-    -p "${DB_PORT}":5432 \
-    -d \
-    --name "postgres_$(date '+%s')" \
-    postgres -N 1000
-  # ^ Increased maximum number of connections for testing purposes
-fi
-
 # Keep pinging Postgres until it's ready to accept commands
 until PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
   echo >&2 "Postgres is still unavailable - sleeping"
