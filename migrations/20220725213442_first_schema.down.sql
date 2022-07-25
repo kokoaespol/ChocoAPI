2826e6090e9ebd3d8aa72140e1ec93d5cf3bb660
Adrián Delgado <11708972+adriandelgado@users.noreply.github.com>
2022-07-25 17:00:08 -0500

    Put schema in first migration (#18)

---
+++ migrations/20220725213442_first_schema.down.sql
@@ -0,0 +1,23 @@
+DROP TABLE posts_tags;
+
+DROP TABLE tags;
+
+DROP TABLE posts;
+
+DROP TABLE users_roles;
+
+DROP TABLE users;
+
+DROP TABLE images;
+
+DROP TABLE image_files;
+
+DROP TABLE image_mime_types;
+
+DROP TABLE emails;
+
+DROP TABLE roles_permissions;
+
+DROP TABLE roles;
+
+DROP TABLE permissions;
